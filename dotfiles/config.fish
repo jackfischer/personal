40acf7122d569f31444096a11e819a78a63acc34
Jack Fischer <jfische8@binghamton.edu>
2016-02-16 10:29:18 -0500

    switch to fish

---
+++ dotfiles/config.fish
@@ -0,0 +1,23 @@
+alias rome "ssh root@jackfischer.me"
+alias wgetmirror "wget -E -H -k -K -p "
+alias wgetreq "wget -p -k"
+alias nmap "nmap -T4 -A -v"
+alias sshdumpster "cat ~/.zshrc|grep sshdumpster"
+alias sshnofap "cat ~/.zshrc|grep sshnofap"
+
+alias g "git"
+alias gs "git status"
+alias gp "git pull"
+alias gpush "git push"
+alias gl "git log"
+alias gc "git commit"
+alias ga "git add"
+alias gcheck "git checkout"
+alias gd "git diff"
+alias gdc "git diff --cached"
+alias gb "git branch"
+alias gr "git remote -v"
+alias py "pyenv versions"
+alias sub "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+
+set -U EDITOR /usr/bin/mvim
